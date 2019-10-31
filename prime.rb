f95bd4e32588a9b6e441049e6f671cf1f7eb0af1
Andres Carvajal <jc1993box@hotmail.com>
2019-10-31 17:14:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+# def prime? (integer)
+#   counter = 0
+#   if integer <= 1
+#     return false 
+#   else integer 
+   
+# end
+# end
+
+def prime?(integer)
+  if integer < 2
+    return false
+  else
+    (2..num / 2).each do |n|
+      return false if num % n == 0
+    end
+  end
+  true
+end 
